c5c73acd5dd747c61b67f770cbf08ca550bc5cfc
Daniel <daniel.votintsev@gmail.com>
2017-11-07 11:30:09 +0200

    Project opening form (RAW)

--- app/components/controls/navBar.jsx
+++ app/components/controls/navBar.jsx
@@ -4,9 +4,18 @@ export default class navBar extends React.Component {
   constructor(props) {
     super(props);
   }
+  openProject(e) {
+    console.log(e);
+  }
   render() {
     return (
       <nav>
+        <input type='file'
+          id='openProject'
+          name='openProject'
+          onChange={(e) => this.openProject(e)}
+          className='open-project' />
+        <label htmlFor='openProject'>+ Add project</label>
       </nav>
     );
   }
